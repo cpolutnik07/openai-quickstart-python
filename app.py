fc2c820f412587b577872ff85fbbaf4a7927d160
cpolutnik07 <cpolutnik07@gmail.com>
2023-01-10 23:22:17 -0700

    envapi

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = ""
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 is_loading = False
